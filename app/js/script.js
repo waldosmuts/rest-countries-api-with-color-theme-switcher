b0c49e56e369939f134befb60012b884ab6bd004
Waldo Smuts <wb.smuts@gmail.com>
2021-12-10 20:36:48 +0200

    added desktop details page

--- app/js/script.js
+++ app/js/script.js
@@ -2,14 +2,14 @@ if ($(".main__countries")[0]) {
     $(".filter__select").hide();
     $.getJSON("https://restcountries.com/v2/all")
         .done(function (data) {
-            for (let i = 0; i < 6; i++) {
+            for (let i = 0; i < 10; i++) {
                 addCountry(data[i]);
             }
-            $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4">Show All</button>');
+            $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4 2xl:col-span-5 md:col-span-2">Show All</button>');
             $(".countries__more").click(function (e) {
                 e.preventDefault();
                 $(this).hide();
-                for (let i = 6; i < data.length; i++) {
+                for (let i = 10; i < data.length; i++) {
                     addCountry(data[i]);
                 }
             })
@@ -34,13 +34,13 @@ if ($(".main__countries")[0]) {
         $(".main__countries").empty();
         $.getJSON(url)
             .done(function (data) {
-                for (let i = 0; i < 6; i++) {
+                for (let i = 0; i < 10; i++) {
                     if (data[i]) {
                         addCountry(data[i]);
                     }
                 }
-                if (data.length > 6) {
-                    $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4">Show All</button>');
+                if (data.length > 10) {
+                    $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4 2xl:col-span-5 md:col-span-2">Show All</button>');
                     $(".countries__more").click(function (e) {
                         e.preventDefault();
                         $(this).hide();
@@ -74,14 +74,14 @@ if ($(".main__countries")[0]) {
         $(".main__countries").empty();
         $.getJSON(`https://restcountries.com/v2/region/${$(this).val()}`)
             .done(function (data) {
-                for (let i = 0; i < 6; i++) {
+                for (let i = 0; i < 10; i++) {
                     addCountry(data[i]);
                 }
-                $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4">Show All</button>');
+                $(".main__countries").append('<button class="countries__more w-full py-3 rounded-md shadow-md text-sm font-semibold lg:col-span-3 xl:col-span-4 2xl:col-span-5 md:col-span-2">Show All</button>');
                 $(".countries__more").click(function (e) {
                     e.preventDefault();
                     $(this).hide();
-                    for (let i = 6; i < data.length; i++) {
+                    for (let i = 10; i < data.length; i++) {
                         addCountry(data[i]);
                     }
                 })
@@ -117,6 +117,9 @@ $(".header__theme").click(function (e) {
 });
 
 function addCountry(country) {
+    if (!country.capital) {
+        country.capital = "N/A";
+    }
     const content = `
     <a class="countries__country rounded-md shadow-md overflow-hidden" href="country.html?country=${country.alpha3Code.toLowerCase()}">
         <img src="${country.flags.png}" class="country__flag">
@@ -135,8 +138,13 @@ function addDetails(country) {
     for (let i = 0; i < country.languages.length; i++) {
         languages += `${country.languages[i].name}, `;
     }
-    for (let i = 0; i < country.currencies.length; i++) {
-        currencies += `${country.currencies[i].name}, `;
+    if (country.currencies) {
+        for (let i = 0; i < country.currencies.length; i++) {
+            currencies += `${country.currencies[i].name}, `;
+        }
+        currencies = currencies.slice(0, -2);
+    } else {
+        currencies = "N/A";
     }
     if (country.borders) {
         for (let i = 0; i < country.borders.length; i++) {
@@ -146,19 +154,27 @@ function addDetails(country) {
                     $(".details__border").append(borderCountry);
                 })
         }
-        borderSection = `<h2 class="text-base mb-4 font-semibold">Border Countries:</h2><div class="details__border flex justify-center gap-2 flex-wrap"></div>`;
+        borderSection = `<h2 class="text-base mb-4 font-semibold">Border Countries:</h2><div class="details__border flex justify-center gap-2 flex-wrap md:col-span-2"></div>`;
     }
-
-    const content = `<img src="${country.flags.png}" class="w-full mb-10">
-    <h1 class="font-extrabold text-xl mb-4">${country.name}</h1>
-    <span class="text-sm mb-2"><span class="font-semibold">Native Name: </span>${country.nativeName}</span>
-    <span class="text-sm mb-2"><span class="font-semibold">Population: </span>${country.population.toLocaleString()}</span>
-    <span class="text-sm mb-2"><span class="font-semibold">Region: </span>${country.region}</span>
-    <span class="text-sm mb-2"><span class="font-semibold">Sub Region:</span> ${country.subregion}</span>
-    <span class="text-sm mb-10"><span class="font-semibold">Capital: </span>${country.capital}</span>
-    <span class="text-sm mb-2"><span class="font-semibold">Top Level Domain: </span>${country.topLevelDomain[0]}</span>
-    <span class="text-sm mb-2"><span class="font-semibold">Currencies: </span>${currencies.slice(0, -2)}</span>
-    <span class="text-sm mb-10"><span class="font-semibold">Languages: </span>${languages.slice(0, -2)}</span>
+    if (!country.capital) {
+        country.capital = "N/A";
+    }
+    const content = `
+    <img src = "${country.flags.png}" class="main__flag w-full mb-10 md:mb-0 md:row-span-4 md:col-span-2 md:pr-32">
+    <h1 class="font-extrabold text-xl mb-4 md:text-3xl md:col-span-2">${country.name}</h1>
+    <div class="flex flex-col mb-10 gap-2 text-sm md:text-base">
+    <span class=""><span class="font-semibold">Native Name: </span>${country.nativeName}</span>
+    <span><span class="font-semibold">Population: </span>${country.population.toLocaleString()}</span>
+    <span><span class="font-semibold">Region: </span>${country.region}</span>
+    <span><span class="font-semibold">Sub Region:</span> ${country.subregion}</span>
+    <span><span class="font-semibold">Capital: </span>${country.capital}</span>
+    </div>
+    <div class="flex flex-col mb-10 gap-2 text-sm md:text-base">
+    <span><span class="font-semibold">Top Level Domain: </span>${country.topLevelDomain[0]}</span>
+    <span><span class="font-semibold">Currencies: </span>${currencies}</span>
+    <span><span class="font-semibold">Languages: </span>${languages.slice(0, -2)}</span>
+    </div>
     ${borderSection}`;
+    // $(`<img src="${country.flags.png}" class="main__flag w-full shadow-md mb-10 md:mr-32  md:w-5/12 md:float-left">`).insertBefore(".main__details");;
     $(".main__details").append(content);
 }
